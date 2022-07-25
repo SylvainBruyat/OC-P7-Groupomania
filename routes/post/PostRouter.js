3906785dae90508e85b973a6be37b3b680594833
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-07-25 15:41:46 +0200

    Correction de la route pour le contrôleur getOnePost

--- routes/post/PostRouter.js
+++ routes/post/PostRouter.js
@@ -8,7 +8,7 @@ const multer = require('../../middleware/multer-config');
 router.post('/', auth, multer, PostController.createPost);
 router.get('/:page', auth, PostController.getFivePosts);
 router.get('/user/:id', auth, PostController.getFivePostsFromUser);
-router.get('/:id', auth, PostController.getOnePost);
+router.get('/single/:id', auth, PostController.getOnePost);
 router.put('/:id', auth, multer, PostController.modifyPost);
 router.delete('/:id', auth, PostController.deletePost);
 router.post('/:id/like', auth, PostController.likePost);

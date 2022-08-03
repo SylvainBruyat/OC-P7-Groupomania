8c4e0883688f56049fe6d004162385beddfa5b39
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-08-03 18:04:57 +0200

    Ajout de la fonctionnalité de modification d'image de profil

--- front/src/pages/Profile.jsx
+++ front/src/pages/Profile.jsx
@@ -4,15 +4,58 @@ import { useParams } from 'react-router-dom';
 import DefaultProfilePicture from '../assets/default-profile-picture.svg';
 import Post from '../components/Post';
 
-let name = ''; // Remplacer par firstName + lastName récupérés de la BDD
+let name = '';
+let profilePicture = '';
 
 export default function Profile() {
     const [customMessage, setCustomMessage] = useState('');
     const [posts, setPosts] = useState([]);
+    const [file, setFile] = useState();
 
     const params = useParams();
-
     const savedToken = sessionStorage.getItem('token');
+
+    function handlePictureUpload(evt) {
+        setFile(evt.target.files[0]);
+    }
+
+    async function handlePictureSubmit(evt) {
+        evt.preventDefault();
+        try {
+            let formData = new FormData();
+
+            formData.append('image', file);
+
+            const response = await fetch(
+                `http://localhost:3000/api/user/${params.id}`,
+                {
+                    method: 'PUT',
+                    headers: {
+                        Authorization: `Bearer ${savedToken}`,
+                    },
+                    body: formData,
+                }
+            );
+            if (response.status === 200) {
+                console.log('Image de profil correctement enregistrée.');
+                //TODO Afficher l'image nouvellement chargée
+                //FetchProfile() n'est pas accessible ici
+            } else if (response.status === 403) {
+                setCustomMessage(
+                    "Vous n'avez pas les droits pour effectuer cette action."
+                );
+            } else if (response.status === 404) {
+                setCustomMessage("Ce profil n'existe pas.");
+            } else if (response.status === 500) {
+                throw new Error(
+                    'Une erreur est survenue côté serveur. Veuillez réessayer ultérieurement.'
+                );
+            } else throw new Error('Erreur inconnue');
+        } catch (error) {
+            setCustomMessage(`${error.message}`);
+            console.error(error);
+        }
+    }
 
     useEffect(() => {
         async function FetchFivePosts() {
@@ -26,6 +69,11 @@ export default function Profile() {
                 if (response.status === 200) {
                     const data = await response.json();
                     setPosts(data); //TODO A corriger : écrase les posts précédents si on en redemande 5 nouveaux
+                    // Créer le nouveau tableau de posts avec Array.prototype.push.apply(posts, data);
+                } else if (response.status === 403) {
+                    setCustomMessage(
+                        "Vous n'avez pas les droits pour accéder à cette ressource."
+                    );
                 } else if (response.status === 404) {
                     setCustomMessage("Cet utilisateur n'existe pas.");
                 } else if (response.status === 500) {
@@ -50,6 +98,7 @@ export default function Profile() {
                 if (response.status === 200) {
                     const data = await response.json();
                     name = `${data.firstName} ${data.lastName}`;
+                    profilePicture = data.profilePictureUrl;
                     FetchFivePosts();
                 } else if (response.status === 403) {
                     setCustomMessage(
@@ -76,21 +125,31 @@ export default function Profile() {
             <p className="custom-message">{customMessage}</p>
             <div className="image-wrapper">
                 <img
-                    src={DefaultProfilePicture}
+                    src={
+                        profilePicture === ''
+                            ? DefaultProfilePicture
+                            : profilePicture
+                    }
                     alt={name}
                     className="profile-picture"
                 />
-                <button>Modifier</button>
+                <form onSubmit={handlePictureSubmit}>
+                    <input
+                        type="file"
+                        accept=".jpg, .jpeg, .png, .gif, .webp"
+                        onChange={handlePictureUpload}
+                    />
+                    <button type="submit">Confirmer</button>
+                </form>
             </div>
             <div className="profile-info">
                 <h1>{name}</h1>
-                {/* Modification du mot de passe. A implémenter plus tard */}
             </div>
             {posts.map((post) => (
                 <Post
                     key={post._id}
                     id={post._id}
-                    author={name}
+                    author={post.userId}
                     userId={post.userId}
                     text={post.text}
                     imageUrl={post.imageUrl}

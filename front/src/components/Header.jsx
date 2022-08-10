dad35f12401180821813e9bcff9f8e80915f7a90
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-08-10 16:02:29 +0200

    Ajout des liens Accueil, Profil et Déconnexion dans le header

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,11 +1,44 @@
+import { useContext } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import { AuthContext } from '../utils/Context';
+
 import redLogo from '../assets/icon-left-font-resized.png';
 
-export default function Header() {
+export default function Header(props) {
+    const userId = props; //TODO props non définies dans App.jsx. Probablement pas possible d'y obtenir l'userId.
+    const { handleLogout } = useContext(AuthContext);
+
     return (
         <header>
-            <a href="/">
-                <img src={redLogo} alt="Logo Groupomania" />
-            </a>
+            <NavLink to="/home">
+                <img src={redLogo} alt="Retour à l'accueil" />
+            </NavLink>
+            <ul className="header-links">
+                <li>
+                    <NavLink
+                        to="/home"
+                        className={({ isActive }) =>
+                            isActive ? 'active-link' : undefined
+                        }
+                    >
+                        Accueil
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink
+                        to={`/profile/${userId}`}
+                        className={({ isActive }) =>
+                            isActive ? 'active-link' : undefined
+                        }
+                    >
+                        Profil
+                    </NavLink>
+                </li>
+            </ul>
+            <button className="logout-button" onClick={() => handleLogout()}>
+                Déconnexion
+            </button>
         </header>
     );
 }

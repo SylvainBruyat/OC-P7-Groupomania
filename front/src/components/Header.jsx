bdbc770420ed20f61ac99756da38dbb1f1dc7316
Sylvain Bruyat <sylvain.bruyat@orange.fr>
2022-09-06 11:13:09 +0200

    Masquage du contenu du Header pour les visiteurs non-connectés

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -10,7 +10,7 @@ import postWriteIcon from '../assets/icons/post-writing-icon.svg';
 import disconnectButton from '../assets/icons/logout-icon.svg';
 
 export default function Header() {
-    const { handleLogout, userId } = useContext(AuthContext);
+    const { handleLogout, token, userId } = useContext(AuthContext);
     const { togglePostPublishMode } = useContext(PostPublishContext);
 
     return (
@@ -22,57 +22,68 @@ export default function Header() {
                     alt="Retour à l'accueil"
                 />
             </NavLink>
-            <ul className="header-links">
-                <li>
-                    <NavLink
-                        to="/home"
-                        className={({ isActive }) =>
-                            isActive ? 'active-link' : undefined
-                        }
+            {token !== null && token !== 'null' && (
+                <>
+                    <ul className="header-links">
+                        <li>
+                            <NavLink
+                                to="/home"
+                                className={({ isActive }) =>
+                                    isActive ? 'active-link' : undefined
+                                }
+                            >
+                                <img
+                                    src={homepageButton}
+                                    alt="Accueil"
+                                    className="homepage-link__icon"
+                                />
+                                <span className="homepage-link__text">
+                                    Accueil
+                                </span>
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink
+                                to={`/profile/${userId}`}
+                                className={({ isActive }) =>
+                                    isActive ? 'active-link' : undefined
+                                }
+                            >
+                                <img
+                                    src={profileButton}
+                                    alt="Profil"
+                                    className="profile-link__icon"
+                                />
+                                <span className="profile-link__text">
+                                    Profil
+                                </span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                    <button
+                        className="post-writing__button"
+                        onClick={togglePostPublishMode}
                     >
                         <img
-                            src={homepageButton}
-                            alt="Accueil"
-                            className="homepage-link__icon"
+                            className="post-writing__icon"
+                            src={postWriteIcon}
+                            alt="Cliquez ici pour écrire un message"
                         />
-                        <span className="homepage-link__text">Accueil</span>
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink
-                        to={`/profile/${userId}`}
-                        className={({ isActive }) =>
-                            isActive ? 'active-link' : undefined
-                        }
+                        <span>Ecrivez quelque chose</span>
+                    </button>
+                    <button
+                        className="logout-button"
+                        onClick={() => handleLogout()}
                     >
                         <img
-                            src={profileButton}
-                            alt="Profil"
-                            className="profile-link__icon"
+                            src={disconnectButton}
+                            alt="Déconnexion"
+                            className="logout-button__icon"
                         />
-                        <span className="profile-link__text">Profil</span>
-                    </NavLink>
-                </li>
-            </ul>
-            <button
-                className="post-writing__button"
-                onClick={togglePostPublishMode}
-            >
-                <img
-                    className="post-writing__icon"
-                    src={postWriteIcon}
-                    alt="Cliquez ici pour écrire un message"
-                />
-                <span>Ecrivez quelque chose</span>
-            </button>
-            <button className="logout-button" onClick={() => handleLogout()}>
-                <img
-                    src={disconnectButton}
-                    alt="Déconnexion"
-                    className="logout-button__icon"
-                />
-                <span className="logout-button__text">Déconnexion</span>
-            </button>
+                        <span className="logout-button__text">Déconnexion</span>
+                    </button>
+                </>
+            )}
         </header>
     );
 }
